177ede1e371bd120f94a6e392ae6666755cd08df
trumaine <trumaine@rogers.com>
2026-04-01 19:52:09 -0400

    initial internal config setup

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/trumaine/blogaggregator/internal/config"
 )
 
 func main() {
-	fmt.Println("hello")
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("lane")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
 }
